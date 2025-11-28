75b3c8dbc813d8be11858420502551c046fb9608
dionis.krasava <dionis.krasava@gmail.com>
2025-11-28 15:05:12 +0700

    mini-mvp-ERR

--- app/src/main/java/app/majo/ui/screens/action_list/ActionListScreen.kt
+++ app/src/main/java/app/majo/ui/screens/action_list/ActionListScreen.kt
@@ -1,11 +1,17 @@
 package app.majo.ui.screens.action_list
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.lazy.items   // ← ЭТО ВАЖНО
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import app.majo.ui.components.ActionCard
+
 
 
 @Composable
