46814d09d3608f8115015988ba1485a6c1869cdb
dionis.krasava <dionis.krasava@gmail.com>
2026-04-09 07:53:59 +0700

    refactor: loading label change (localization)

--- app/src/main/java/app/majo/MainActivity.kt
+++ app/src/main/java/app/majo/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import app.majo.ui.MainScreen
@@ -85,7 +86,7 @@ class MainActivity : ComponentActivity() {
                 }
             } else {
                 // Показываем загрузку
-                Text("Download...")
+                Text(stringResource(R.string.loading))
             }
 
 
